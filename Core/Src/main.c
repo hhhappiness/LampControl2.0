7c761275379d4f4e8c4ac823d54b032cbedc5a6a
ody <kiwikiwishit@gmail.com>
2025-05-07 17:54:24 +0800

    Add extended SPI HAL module driver with RX FIFO flush functionality
    This commit introduces the stm32g4xx_hal_spi_ex.c file, which provides an extended SPI HAL module driver for the STM32G4 series. The new functionality includes a function to flush the RX FIFO, allowing for better management of SPI data transfers. The HAL_SPIEx_FlushRxFifo function checks the RX FIFO level and clears it if necessary, returning a status indicating success or timeout. This enhancement improves the robustness of SPI communication in applications using the STM32G4 microcontrollers.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,7 +24,7 @@
 
 ADC_HandleTypeDef hadc2;
 RTC_HandleTypeDef hrtc;
-
+SPI_HandleTypeDef hspi2;
 WWDG_HandleTypeDef hwwdg;
 
 void SystemClock_Config(void);
@@ -32,6 +32,7 @@ static void MX_GPIO_Init(void);
 static void MX_ADC2_Init(void);
 static void MX_WWDG_Init(void);
 static void MX_RTC_Init(void);
+static void MX_SPI2_Init(void);
 void HardwareInit(void);
 void Init(void);
 
@@ -196,7 +197,46 @@ static void MX_ADC2_Init(void)
    /* USER CODE END RTC_Init 2 */
  
  }
-
+/**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+ static void MX_SPI2_Init(void)
+ {
+ 
+   /* USER CODE BEGIN SPI2_Init 0 */
+ 
+   /* USER CODE END SPI2_Init 0 */
+ 
+   /* USER CODE BEGIN SPI2_Init 1 */
+ 
+   /* USER CODE END SPI2_Init 1 */
+   /* SPI2 parameter configuration*/
+   hspi2.Instance = SPI2;
+   hspi2.Init.Mode = SPI_MODE_MASTER;
+   hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+   hspi2.Init.DataSize = SPI_DATASIZE_4BIT;
+   hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+   hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+   hspi2.Init.NSS = SPI_NSS_HARD_OUTPUT;
+   hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+   hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+   hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+   hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+   hspi2.Init.CRCPolynomial = 7;
+   hspi2.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+   hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+   if (HAL_SPI_Init(&hspi2) != HAL_OK)
+   {
+     Error_Handler();
+   }
+   /* USER CODE BEGIN SPI2_Init 2 */
+ 
+   /* USER CODE END SPI2_Init 2 */
+ 
+ }
+ 
 /**
   * @brief WWDG Initialization Function
   * @param None
@@ -302,13 +342,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LCD_CSB_Pin LCD_SCL_Pin LCD_SDA_Pin */
-  GPIO_InitStruct.Pin = LCD_CSB_Pin|LCD_SCL_Pin|LCD_SDA_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  // /*Configure GPIO pins : LCD_CSB_Pin LCD_SCL_Pin LCD_SDA_Pin */
+  // GPIO_InitStruct.Pin = LCD_CSB_Pin|LCD_SCL_Pin|LCD_SDA_Pin;
+  // GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  // GPIO_InitStruct.Pull = GPIO_NOPULL;
+  // GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  // GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
+  // HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pins : KEY_CHA_Pin KEY_CHB_Pin */
   GPIO_InitStruct.Pin = KEY_CHA_Pin|KEY_CHB_Pin;
@@ -342,6 +382,7 @@ void HardwareInit()
   MX_ADC2_Init();             //ADC初始化
   MX_WWDG_Init();             //看门狗初始化
   MX_RTC_Init();           //RTC初始化
+  MX_SPI2_Init();
 
 }
 
